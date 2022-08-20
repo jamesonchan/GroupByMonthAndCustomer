336a3223a2d9140680e04e7e41e7bb2269ce2c73
Jameson Chen <[jamesonychan@gmail.com]>
2022-08-19 22:07:22 -0400

    added api call test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import * as Api from "./services/transaction.api";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// jest.mock("./services/transaction.api");
+const mockData = [{ customerId: 1, createDate: 165733232 }];
+Api.fetchTransactionMockRecord = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res(mockData);
+    }, 1000);
+  });
+};
+
+describe("App", () => {
+  it("should match snapshot", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("response from mock api", () => {
+    expect.assertions(1);
+    return Api.fetchTransactionMockRecord().then((data) => {
+      expect(data).toBe(mockData);
+    });
+  });
 });
